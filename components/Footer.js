8f8c4a8eeb3c56bf3c78cbc4ff766f69258b1b69
oguzdurmaz-dev <oz@ozzdigitalworks.com>
2021-05-25 21:34:50 +0300

    Footer.js and Footer.module.css

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,17 @@
-import styles from "@/styles/Footer.module.css"
+import styles from "@/styles/Footer.module.css";
 import { useContext } from "react";
 import AppContext from "@/context/AppContext";
 
-export default function FooterPage() {  
-    const { currentStation } = useContext(AppContext); 
-    return (
-        <div className={styles.footer}>
-          
-         
-      
-
-        </div>
-    )
+export default function FooterPage() {
+  const { currentStation } = useContext(AppContext);
+  return (
+    <div className={styles.footer}>
+      {currentStation ? (
+        <>
+          <span className={styles.subHead}>CURRENTLY PLAYING</span>
+          <h4 className={styles.current}>{currentStation}</h4>
+        </>
+      ) : null}
+    </div>
+  );
 }
